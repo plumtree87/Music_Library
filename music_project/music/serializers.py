faa30edf2001204c398f8dfc916046004037e14a
plumtree87 <plumtree87@protonmail.com>
2021-04-16 21:31:32 +0500

    added / at the end of my int parameter

--- music_project/music/serializers.py
+++ music_project/music/serializers.py
@@ -7,13 +7,32 @@ class SongSerializer(serializers.ModelSerializer):
         model = Song
         fields = ['id', 'title', 'artist', 'album', 'release_date']
 
-    def create(self, validated_data):
-        return Song.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.artist = validated_data.get('artist', instance.artist)
-        instance.album = validated_data.get('album', instance.album)
-        instance.release_date = validated_data.get('release_date', instance.release_date)
-        instance.save()
-        return instance
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # kkeeping these here for reference.
+    #
+    # def create(self, validated_data):
+    #     return Song.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.artist = validated_data.get('artist', instance.artist)
+    #     instance.album = validated_data.get('album', instance.album)
+    #     instance.release_date = validated_data.get('release_date', instance.release_date)
+    #     instance.save()
+    #     return instance
